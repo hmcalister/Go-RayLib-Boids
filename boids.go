60d742ecf0dec9e30cdfca61f9cf325947ce6e06
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-26 02:52:27 +1300

    Update boid drawing constants

--- boids.go
+++ boids.go
@@ -15,8 +15,8 @@ const (
 
 var (
 	BOID_COLOR       color.RGBA = rl.White
-	BOID_SIDE_LENGTH float32    = 16.0
-	BOID_SIDE_ANGLE  float32    = 2.8
+	BOID_SIDE_LENGTH float32    = 8.0
+	BOID_SIDE_ANGLE  float32    = 2.3
 )
 
 type Boid struct {
