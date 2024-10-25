12806f23e163231c2c8fa71676248744178fe647
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-26 00:27:09 +1300

    Add Normalize method to CartesianVector2

--- vector.go
+++ vector.go
@@ -11,3 +11,11 @@ func (v CartesianVector2) Magnitude() float64 {
 	return math.Sqrt(v.DotProduct(v))
 }
 
+func (v CartesianVector2) Normalize() CartesianVector2 {
+	mag := v.Magnitude()
+	return CartesianVector2{
+		v.X / mag,
+		v.Y / mag,
+	}
+}
+
