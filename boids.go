2bc0ae5405d483c581b8f7c27ca7d70a60529769
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-25 23:52:50 +1300

    Set random seed based on timestamp if exactly 0

--- boids.go
+++ boids.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log/slog"
+	"time"
+
 	"golang.org/x/exp/rand"
 )
 
@@ -23,4 +26,11 @@ type BoidManager struct {
 // randomSeed is used to initialize the Boids. If passed randomSeed is exactly 0 then a new seed is generated based on the timestamp.
 // Boids are initialized randomly with both velocity and position.
 func NewBoidManager(numBoids int, randomSeed uint64) BoidManager {
+	slog.Debug("start BoidManager initialization")
+
+	manager := BoidManager{}
+	if randomSeed == 0 {
+		randomSeed = uint64(time.Now().UnixMicro())
+		slog.Info("randomSeed set based on timestamp", "randomSeed", randomSeed)
+	}
 }
