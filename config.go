8539a3bfbfddaf25d29e5cb69d1217b76669b92a
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-25 22:34:00 +1300

    Add logical checks for config data

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log/slog"
 	"os"
 
@@ -32,6 +33,31 @@ func ParseConfigFile(configFilePath string) (Config, error) {
 		slog.Error("error unmarshalling config file", "configFilePath", configFilePath, "error", err)
 		return config, err
 	}
+
+	// --------------------------------------------------------------------------------
+	// Check logical values of config data
+
+	if config.WindowWidth <= 0 {
+		slog.Error("window width must be positive", "parsedWindowWidth", config.WindowWidth)
+		return config, errors.New("window width must be positive")
+	}
+
+	if config.WindowHeight <= 0 {
+		slog.Error("window height must be positive", "parsedWindowHeight", config.WindowHeight)
+		return config, errors.New("window height must be positive")
+	}
+
+	if config.NumWorkers <= 0 {
+		slog.Error("num workers must be positive", "parsedNumWorkers", config.NumWorkers)
+		return config, errors.New("num workers must be positive")
+	}
+
+	if config.NumBoids <= 0 {
+		slog.Error("num boids must be positive", "parsedNumBoids", config.NumBoids)
+		return config, errors.New("num boids must be positive")
+	}
+
+	// --------------------------------------------------------------------------------
 
 	return config, nil
 }
