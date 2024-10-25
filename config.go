4efccd76ca7e1709f90a9d5ceb2d09dd313eed3a
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-25 23:51:11 +1300

    Update random seed to use uint64 type

--- config.go
+++ config.go
@@ -9,11 +9,11 @@ import (
 )
 
 type Config struct {
-	WindowWidth  int32 `yaml:"WindowWidth"`
-	WindowHeight int32 `yaml:"WindowHeight"`
-	NumBoids     int   `yaml:"NumBoids"`
-	NumWorkers   int   `yaml:"NumWorkers"`
-	RandomSeed   int64 `yaml:"RandomSeed"`
+	WindowWidth  int32  `yaml:"WindowWidth"`
+	WindowHeight int32  `yaml:"WindowHeight"`
+	NumBoids     int    `yaml:"NumBoids"`
+	NumWorkers   int    `yaml:"NumWorkers"`
+	RandomSeed   uint64 `yaml:"RandomSeed"`
 }
 
 func ParseConfigFile(configFilePath string) (Config, error) {
