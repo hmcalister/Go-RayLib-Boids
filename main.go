4c49c69747d33eb959a54ee78c1f5b24dd5374e6
Hayden McAlister <haydenmcalister49@gmail.com>
2024-10-25 23:54:27 +1300

    Update default slog level to INFO for output messages

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	rlLogLevelFlag := flag.String("rlLogLevel", "none", "Set the raylib log level. Valid values are: fatal, error, warning, info, debug, trace, none.")
-	slogLevelFlag := flag.String("slogLevel", "none", "Set the slog level. Valid values are: fatal, error, warning, info, debug, trace, none.")
+	slogLevelFlag := flag.String("slogLevel", "info", "Set the slog level. Valid values are: fatal, error, warning, info, debug, trace, none.")
 	slogFormatFlag := flag.String("slogFormat", "pretty", "Set the slog format. Valid values are: text, pretty, json.")
 	configFilePath := flag.String("configFilePath", "config.yaml", "Path to the config file to use.")
 	flag.Parse()
